983674a13ae816562c5a8098b235c75bad6cec13
Ioannis Tziakos <ioannist@enthought.com>
2014-02-17 23:30:25 +0000

    fix the line numbers

--- pikos/tests/test_focused_function_memory_monitor.py
+++ pikos/tests/test_focused_function_memory_monitor.py
@@ -7,18 +7,16 @@
 #  Copyright (c) 2012, Enthought, Inc.
 #  All rights reserved.
 #------------------------------------------------------------------------------
-import StringIO
 import unittest
 
 from pikos.filters.on_value import OnValue
 from pikos.monitors.focused_function_memory_monitor import (
     FocusedFunctionMemoryMonitor)
 from pikos.recorders.list_recorder import ListRecorder
-from pikos.tests.test_assistant import TestAssistant
 from pikos.tests.compat import TestCase
 
 
-class TestFocusedFunctionMemoryMonitor(TestCase, TestAssistant):
+class TestFocusedFunctionMemoryMonitor(TestCase):
 
     def setUp(self):
         self.filename = __file__.replace('.pyc', '.py')
@@ -53,12 +51,12 @@ class TestFocusedFunctionMemoryMonitor(TestCase, TestAssistant):
         boo()
         self.assertEqual(result, 3)
         expected = [
-            "0 call gcd 30 {0}".format(self.filename),
-            "1 call internal 35 {0}".format(self.filename),
-            "2 return internal 36 {0}".format(self.filename),
-            "3 call internal 35 {0}".format(self.filename),
-            "4 return internal 36 {0}".format(self.filename),
-            "5 return gcd 33 {0}".format(self.filename)]
+            "0 call gcd 28 {0}".format(self.filename),
+            "1 call internal 33 {0}".format(self.filename),
+            "2 return internal 34 {0}".format(self.filename),
+            "3 call internal 33 {0}".format(self.filename),
+            "4 return internal 34 {0}".format(self.filename),
+            "5 return gcd 31 {0}".format(self.filename)]
         records = self.get_records(recorder)
         self.assertEqual(records, expected)
         self.assertEqual(logger._code_trackers, {})
@@ -98,18 +96,18 @@ class TestFocusedFunctionMemoryMonitor(TestCase, TestAssistant):
         boo()
         self.assertEqual(result, 3)
         expected = [
-            "0 call internal 73 {0}".format(self.filename),
-            "1 return internal 74 {0}".format(self.filename),
-            "2 call internal 73 {0}".format(self.filename),
-            "3 return internal 74 {0}".format(self.filename),
-            "4 call foo 79 {0}".format(self.filename),
-            "5 call boo 76 {0}".format(self.filename),
-            "6 return boo 77 {0}".format(self.filename),
-            "7 call boo 76 {0}".format(self.filename),
-            "8 return boo 77 {0}".format(self.filename),
-            "9 call boo 76 {0}".format(self.filename),
-            "10 return boo 77 {0}".format(self.filename),
-            "11 return foo 82 {0}".format(self.filename)]
+            "0 call internal 71 {0}".format(self.filename),
+            "1 return internal 72 {0}".format(self.filename),
+            "2 call internal 71 {0}".format(self.filename),
+            "3 return internal 72 {0}".format(self.filename),
+            "4 call foo 77 {0}".format(self.filename),
+            "5 call boo 74 {0}".format(self.filename),
+            "6 return boo 75 {0}".format(self.filename),
+            "7 call boo 74 {0}".format(self.filename),
+            "8 return boo 75 {0}".format(self.filename),
+            "9 call boo 74 {0}".format(self.filename),
+            "10 return boo 75 {0}".format(self.filename),
+            "11 return foo 80 {0}".format(self.filename)]
         records = self.get_records(recorder)
         self.assertEqual(records, expected)
         self.assertEqual(logger._code_trackers, {})
@@ -142,14 +140,14 @@ class TestFocusedFunctionMemoryMonitor(TestCase, TestAssistant):
         boo()
         self.assertEqual(result, 3)
         expected = [
-            "0 call gcd 119 {0}".format(self.filename),
-            "1 call foo 126 {0}".format(self.filename),
-            "2 return foo 127 {0}".format(self.filename),
-            "3 call gcd 119 {0}".format(self.filename),
-            "4 call foo 126 {0}".format(self.filename),
-            "5 return foo 127 {0}".format(self.filename),
-            "6 return gcd 121 {0}".format(self.filename),
-            "7 return gcd 121 {0}".format(self.filename)]
+            "0 call gcd 117 {0}".format(self.filename),
+            "1 call foo 124 {0}".format(self.filename),
+            "2 return foo 125 {0}".format(self.filename),
+            "3 call gcd 117 {0}".format(self.filename),
+            "4 call foo 124 {0}".format(self.filename),
+            "5 return foo 125 {0}".format(self.filename),
+            "6 return gcd 119 {0}".format(self.filename),
+            "7 return gcd 119 {0}".format(self.filename)]
         records = self.get_records(recorder)
         self.assertEqual(records, expected)
         self.assertEqual(logger._code_trackers, {})
@@ -170,14 +168,14 @@ class TestFocusedFunctionMemoryMonitor(TestCase, TestAssistant):
         result = gcd(12, 3)
         self.assertEqual(result, 3)
         expected = [
-            "0 call gcd 165 {0}".format(self.filename),
-            "1 call foo 159 {0}".format(self.filename),
-            "2 return foo 160 {0}".format(self.filename),
-            "10 call gcd 165 {0}".format(self.filename),
-            "11 call foo 159 {0}".format(self.filename),
-            "12 return foo 160 {0}".format(self.filename),
-            "13 return gcd 168 {0}".format(self.filename),
-            "21 return gcd 168 {0}".format(self.filename)]
+            "0 call gcd 163 {0}".format(self.filename),
+            "1 call foo 157 {0}".format(self.filename),
+            "2 return foo 158 {0}".format(self.filename),
+            "10 call gcd 163 {0}".format(self.filename),
+            "11 call foo 157 {0}".format(self.filename),
+            "12 return foo 158 {0}".format(self.filename),
+            "13 return gcd 166 {0}".format(self.filename),
+            "21 return gcd 166 {0}".format(self.filename)]
         records = self.get_records(recorder)
         self.assertEqual(records, expected)
         self.assertEqual(logger._code_trackers, {})
