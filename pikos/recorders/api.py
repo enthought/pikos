258d7765c6f76b800ce1205c7f80a41b0b820b25
Ioannis Tziakos <ioannist@enthought.com>
2014-02-20 23:12:58 +0000

    add the new recorder to the api file

--- pikos/recorders/api.py
+++ pikos/recorders/api.py
@@ -1,4 +1,6 @@
 from pikos.recorders.list_recorder import ListRecorder
+from pikos.recorders.text_file_recorder import TextFileRecorder
+from pikos.recorders.csv_file_recorder import CSVFileRecorder
 from pikos.recorders.csv_recorder import CSVRecorder
 from pikos.recorders.text_stream_recorder import TextStreamRecorder
 from pikos.recorders.zeromq_recorder import ZeroMQRecorder
