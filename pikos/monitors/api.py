855cfc6582630089cd54db7748856d3ac314386a
Ioannis Tziakos <ioannist@enthought.com>
2014-05-01 20:27:52 +0100

    do not attempt to load monitors that depend on psutils when the package is not there

--- pikos/monitors/api.py
+++ pikos/monitors/api.py
@@ -19,15 +19,23 @@ __all__ = [
     'MonitorAttach',
     'Monitor'
 ]
+
 from pikos.monitors.function_monitor import FunctionMonitor
 from pikos.monitors.line_monitor import LineMonitor
-from pikos.monitors.function_memory_monitor import FunctionMemoryMonitor
-from pikos.monitors.line_memory_monitor import LineMemoryMonitor
 from pikos.monitors.focused_function_monitor import FocusedFunctionMonitor
 from pikos.monitors.focused_line_monitor import FocusedLineMonitor
-from pikos.monitors.focused_function_memory_monitor import \
-    FocusedFunctionMemoryMonitor
-from pikos.monitors.focused_line_memory_monitor import \
-    FocusedLineMemoryMonitor
 from pikos.monitors.monitor_attach import MonitorAttach
 from pikos.monitors.monitor import Monitor
+
+try:
+    import psutil
+except ImportError:
+    import warnings
+    warnings.warn('Could not import psutil. Memory monitors are not available')
+else:
+    from pikos.monitors.function_memory_monitor import FunctionMemoryMonitor
+    from pikos.monitors.line_memory_monitor import LineMemoryMonitor
+    from pikos.monitors.focused_function_memory_monitor import \
+        FocusedFunctionMemoryMonitor
+    from pikos.monitors.focused_line_memory_monitor import \
+        FocusedLineMemoryMonitor
