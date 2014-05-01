498c810a4d8808be3b217f33646ce70c4e7de4ee
Ioannis Tziakos <ioannist@enthought.com>
2014-05-01 21:11:51 +0100

    update copyright notice

--- pikos/tests/test_focused_function_memory_monitor.py
+++ pikos/tests/test_focused_function_memory_monitor.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #------------------------------------------------------------------------------
 #  Package: Pikos toolkit
-#  File: monitors/test_focused_function_monitor.py
+#  File: monitors/test_focused_function_memory_monitor.py
 #  License: LICENSE.TXT
 #
 #  Copyright (c) 2012, Enthought, Inc.
