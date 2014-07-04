6b4d31d10230b5523fb91978970ff14578737c48
Ioannis Tziakos <ioannist@enthought.com>
2014-07-04 22:53:24 +0100

    add a cython version for the FocusedFunctionMonitor

--- pikos/cymonitors/api.py
+++ pikos/cymonitors/api.py
@@ -1,8 +1,10 @@
 __all__ = [
     'Monitor',
+    'FocusedFunctionMonitor',
     'FunctionMonitor',
     'FunctionMemoryMonitor',
 ]
 from pikos.cymonitors.monitor import Monitor
+from pikos.cymonitors.focused_function_monitor import FocusedFunctionMonitor
 from pikos.cymonitors.function_monitor import FunctionMonitor
 from pikos.cymonitors.function_memory_monitor import FunctionMemoryMonitor
--- setup.py
+++ setup.py
@@ -52,6 +52,10 @@ cython_monitors = Feature(
             sources=[
                 'pikos/cymonitors/function_monitor.pyx']),
         Extension(
+            'pikos.cymonitors.focused_function_monitor',
+            sources=[
+                'pikos/cymonitors/focused_function_monitor.pyx']),
+        Extension(
             'pikos.cymonitors.function_memory_monitor',
             sources=[
                 'pikos/cymonitors/function_memory_monitor.pyx'])])
