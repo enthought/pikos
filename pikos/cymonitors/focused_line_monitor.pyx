502c8f4703028a802ca1ee27cfd0806899f2c4a8
Ioannis Tziakos <ioannist@enthought.com>
2014-08-24 01:00:14 +0100

    fix typo

--- pikos/cymonitors/focused_line_monitor.pyx
+++ pikos/cymonitors/focused_line_monitor.pyx
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #----------------------------------------------------------------------------
 #  Package: Pikos toolkit
-#  File: cymonitors/focused_line_monitor.py
+#  File: cymonitors/focused_line_monitor.pyx
 #  License: LICENSE.TXT
 #
 #  Copyright (c) 2014, Enthought, Inc.
