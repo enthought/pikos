348e97524e108a617fe7233f225dee388953d43e
Ioannis Tziakos <ioannist@enthought.com>
2014-10-04 02:46:37 +0100

    move sdk set ver to the batch files

--- appveyor-install.cmd
+++ appveyor-install.cmd
@@ -1,3 +1,4 @@
+%sdkver% -q -version:v7.0
 setenv /x64 || exit 1
 if %cython% EQU "true" pip install cython || exit 1
 pip install -r requirements.txt" || exit 1
--- appveyor-test.cmd
+++ appveyor-test.cmd
@@ -1,2 +1,3 @@
+%sdkver% -q -version:v7.0
 setenv /x64 || exit 1
 python setup.py test || exit 1
