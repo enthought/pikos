e4e84a5ff3cf1c3735c671d30e4cfb58568d2b0a
Ioannis Tziakos <ioannist@enthought.com>
2014-10-04 03:47:55 +0100

    fix install batch

--- appveyor-install.cmd
+++ appveyor-install.cmd
@@ -1,4 +1,4 @@
-"%sdkver%" -q -version:v7.0 || exit 1
-setenv /x64 || exit 1
-if %cython% EQU "true" pip install cython || exit 1
-pip install -r requirements.txt" || exit 1
+"%sdkver%" -q -version:v7.0
+setenv /x64
+if %cython% EQU "true" pip install cython
+pip install -r requirements.txt
