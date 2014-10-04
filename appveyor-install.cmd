b95cfcc213991d14e0dd39051ace56f88b39ff66
Ioannis Tziakos <ioannist@enthought.com>
2014-10-04 03:29:50 +0100

    fix call to sdkver in batch files

--- appveyor-install.cmd
+++ appveyor-install.cmd
@@ -1,4 +1,4 @@
-%sdkver% -q -version:v7.0
+"%sdkver%" -q -version:v7.0 || exit 1
 setenv /x64 || exit 1
 if %cython% EQU "true" pip install cython || exit 1
 pip install -r requirements.txt" || exit 1
--- appveyor-test.cmd
+++ appveyor-test.cmd
@@ -1,3 +1,3 @@
-%sdkver% -q -version:v7.0
+"%sdkver%" -q -version:v7.0 || exit 1
 setenv /x64 || exit 1
 python setup.py test || exit 1
