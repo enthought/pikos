a3ab1aea1d992a029ef0d870e3ff95abcf2e3ff5
Ioannis Tziakos <ioannist@enthought.com>
2014-10-04 04:22:31 +0100

    fix the python path

--- appveyor-install.cmd
+++ appveyor-install.cmd
@@ -1,4 +1,4 @@
 "%sdkver%" -q -version:v7.0
 call setenv /x64
-if %cython%=='true' ("%pip%" install cython)
-"%pip%" install -r requirements.txt
+if %cython%=='true' (pip install cython)
+pip install -r requirements.txt
--- appveyor-test.cmd
+++ appveyor-test.cmd
@@ -1,4 +1,3 @@
 "%sdkver%" -q -version:v7.0
 call setenv /x64
-python -version
 python setup.py test || exit 1
